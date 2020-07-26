481b026e9bbe721743bd1f0b3e6b70f6f4aa1521
syedre <syed.rehan123@gmail.com>
2020-07-26 16:22:54 +0530

    Updates

---
+++ precache-manifest.eaab46d8c9ed18722f4a28bf6e837ee6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbbda839d1a5580792162ccb803101c",
+    "url": "/ui/index.html"
+  },
+  {
+    "revision": "12fbb80e419516944a66",
+    "url": "/ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ed4d647f22b65a85852",
+    "url": "/ui/static/js/2.8c7439e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ui/static/js/2.8c7439e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12fbb80e419516944a66",
+    "url": "/ui/static/js/main.864fc596.chunk.js"
+  },
+  {
+    "revision": "c445eb6c667fa1f71172",
+    "url": "/ui/static/js/runtime-main.6e6f91d1.js"
+  },
+  {
+    "revision": "9befeb30447e3055e285cef65417ac1c",
+    "url": "/ui/static/media/avatar.9befeb30.jpg"
+  },
+  {
+    "revision": "49caa96505dcce42630ffa84ac6a1a43",
+    "url": "/ui/static/media/graph.49caa965.jpg"
+  }
+]);
